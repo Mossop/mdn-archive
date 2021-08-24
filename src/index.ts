53f448a67fb26d8f8a26a3125531f553225c2b1d
Dave Townsend <dtownsend@oxymoronical.com>
2021-08-24 11:52:59 +0100

    Initial webserver.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello");
+import koa from "koa";
+import type Koa from "koa";
+
+async function init(): Promise<void> {
+  let port = 8000;
+  if (process.argv.length >= 3) {
+    port = parseInt(process.argv[2]);
+  }
+
+  let app = new koa();
+
+  app.use(async (ctx: Koa.DefaultContext, next: Koa.Next): Promise<void> => {
+    await next();
+  });
+
+  app.listen(port);
+
+  console.log(`Listening on http://localhost:${port}`);
+}
+
+init().catch((e: Error) => console.error(e));
